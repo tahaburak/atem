b1a9bd3488afb2648cd563919dd7ec8ce97b6135
Taha Burak <tahaburak.koc@gmail.com>
2019-11-03 23:04:42 +0300

    initial commit

---
+++ src/main/java/com/tahaburak/atem/AtemApplication.java
@@ -0,0 +1,13 @@
+package com.tahaburak.atem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tahaburak/atem/AtemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tahaburak.atem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
