02e289467b17666ab4f63d5c9c0892dcd27d3d59
Taha Burak <tahaburak.koc@gmail.com>
2020-01-12 23:08:08 +0300

    integrated lombok

--- src/main/java/com/tahaburak/atem/model/User.java
+++ src/main/java/com/tahaburak/atem/model/User.java
@@ -1,56 +1,20 @@
 package com.tahaburak.atem.model;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.util.Date;
 
 /**
  * Created by burak on 24.11.2019
  */
+@Data
+@Builder
 public class User {
 
-	private Long id;
+  private Long id;
 
-	private String name;
-	private String surname;
-	private Date birthDate;
-
-	public User(Long id, String name, String surname, Date birthDate) {
-		this.id = id;
-		this.name = name;
-		this.surname = surname;
-		this.birthDate = birthDate;
-	}
-
-	// long list of getters and setters
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-
-	public Date getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
+  private String name;
+  private String surname;
+  private Date birthDate;
 }
--- src/main/java/com/tahaburak/atem/service/impl/DummyService.java
+++ src/main/java/com/tahaburak/atem/service/impl/DummyService.java
@@ -3,6 +3,7 @@ package com.tahaburak.atem.service.impl;
 import com.github.javafaker.Faker;
 import com.tahaburak.atem.model.User;
 import com.tahaburak.atem.service.IDummyService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -11,21 +12,24 @@ import java.util.Locale;
 /**
  * Created by burak on 25.11.2019
  */
+@Slf4j
 @Service
 public class DummyService implements IDummyService {
-	private Faker faker;
+  private Faker faker;
 
-	@PostConstruct
-	private void fill() {
-		this.faker = generateFaker();
-	}
+  @PostConstruct
+  private void fill() {
+    this.faker = generateFaker();
+  }
 
-	private Faker generateFaker() {
-		return new Faker(new Locale("en-US"));
-	}
+  private Faker generateFaker() {
+    return new Faker(new Locale("en-US"));
+  }
 
-	@Override
-	public User getDummyUserById(Long id) {
-		return new User(id, faker.name().firstName(), faker.name().lastName(), faker.date().birthday());
-	}
+  @Override
+  public User getDummyUserById(Long id) {
+    log.info("generating the dummy user with id: " + id);
+    return User.builder().name(faker.name().firstName()).surname(faker.name().lastName()).build();
+
+  }
 }
